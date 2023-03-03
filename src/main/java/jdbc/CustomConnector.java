5b4e64bdc056b516530488bc392c823aa04f4f79
tabletka97 <mtabletka.by@gmail.com>
2023-03-03 17:01:08 +0300

    done?

--- src/main/java/jdbc/CustomConnector.java
+++ src/main/java/jdbc/CustomConnector.java
@@ -1,11 +1,22 @@
 package jdbc;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class CustomConnector {
-    public Connection getConnection(String url) {
+    private final ResourceBundle rb = ResourceBundle.getBundle("app");
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(rb.getString("postgres.driver"));
+        return DriverManager.getConnection(rb.getString("postgres.url"),
+        rb.getString("postgres.name"), rb.getString("postgres.password"));
     }
 
-    public Connection getConnection(String url, String user, String password)  {
+    public Connection getConnection(String url, String user, String password)
+            throws ClassNotFoundException, SQLException {
+        Class.forName(rb.getString("postgres.driver"));
+        return DriverManager.getConnection(url, user, password);
     }
 }
--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -6,9 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -21,28 +20,119 @@ public class SimpleJDBCRepository {
     private PreparedStatement ps = null;
     private Statement st = null;
 
-    private static final String createUserSQL = "";
-    private static final String updateUserSQL = "";
-    private static final String deleteUser = "";
-    private static final String findUserByIdSQL = "";
-    private static final String findUserByNameSQL = "";
-    private static final String findAllUserSQL = "";
+    private static final String createUserSQL = "insert into myusers (id, firstname, lastname, age) values ((?), (?), (?), (?)) returning id";
+    private static final String updateUserSQL = "update myusers set id = (?), firstname = (?), lastname = (?), age = (?) where id = (?) returning id, firstname, lastname, age";
+    private static final String deleteUser = "delete from myusers where id = (?)";
+    private static final String findUserByIdSQL = "select * from myusers where id = (?)";
+    private static final String findUserByNameSQL = "select * from myusers where firstname = (?)";
+    private static final String findAllUserSQL = "select * from myusers";
 
-    public Long createUser() {
+    public Long createUser(User user) {
+        long result = -1;
+        try {
+            ps = connection.prepareStatement(createUserSQL);
+            ps.setLong(1, user.getId());
+            ps.setString(2, user.getFirstName());
+            ps.setString(3, user.getLastName());
+            ps.setInt(4, user.getAge());
+            try (ResultSet resultSet = ps.executeQuery()) {
+                if (resultSet.next()) {
+                    result = resultSet.getLong(1);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     public User findUserById(Long userId) {
+        User resultUser = null;
+        try {
+            ps = connection.prepareStatement(findUserByIdSQL);
+            ps.setLong(1, userId);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    resultUser = new User(rs.getLong(1),
+                            rs.getString(2),
+                            rs.getString(3),
+                            rs.getInt(4));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultUser;
     }
 
     public User findUserByName(String userName) {
+        User resultUser = null;
+        try {
+            ps = connection.prepareStatement(findUserByNameSQL);
+            ps.setString(1, userName);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    resultUser = new User(rs.getLong(1),
+                            rs.getString(2),
+                            rs.getString(3),
+                            rs.getInt(4));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultUser;
     }
 
     public List<User> findAllUser() {
+        List<User> resultList = new ArrayList<>();
+        try {
+            ps = connection.prepareStatement(findAllUserSQL);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    resultList.add(new User(rs.getLong(1),
+                            rs.getString(2),
+                            rs.getString(3),
+                            rs.getInt(4)));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultList;
     }
 
-    public User updateUser() {
+    public User updateUser(User user) {
+        User resultUser = null;
+        try {
+            ps = connection.prepareStatement(updateUserSQL);
+            ps.setLong(1, user.getId());
+            ps.setLong(5, user.getId());
+            ps.setString(2, user.getFirstName());
+            ps.setString(3, user.getLastName());
+            ps.setInt(4, user.getAge());
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    resultUser = new User(rs.getLong(1),
+                            rs.getString(2),
+                            rs.getString(3),
+                            rs.getInt(4));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultUser;
     }
 
     private void deleteUser(Long userId) {
+        try {
+            ps = connection.prepareStatement(deleteUser);
+            ps.setLong(1, userId);
+            ps.executeUpdate();
+            System.out.println("User with id " + userId + " successfully deleted");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
